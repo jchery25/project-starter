06119bbb9b725d6e3e9aef33809f83d1b5c3d9fe
Mike <60683751+Al-toid@users.noreply.github.com>
2024-11-13 20:31:38 -0500

    Date format update

--- frontend-client/src/components/MicroPostCard.jsx
+++ frontend-client/src/components/MicroPostCard.jsx
@@ -1,13 +1,21 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import { formatDistanceToNow, toDate } from "date-fns";
+
 
 function MicroPostCard({ content, createdAt, id }) {
+
+  const result = toDate(createdAt)
+
+  // const estDate = utcToZonedTime(parseISO(createdAt), "America/New_York");
+  const relativeTime = formatDistanceToNow(result, { addSuffix: true });
+
   return (
     <div className="col-10 col-md-8 col-lg-7">
       <div className="card mb-4 shadow">
         <div className="card-body card-text">
           <Link to={"/posts/" + id}>{content}</Link>
         </div>
-        <div className="card-footer small text-muted text-end">{createdAt}</div>
+        <div className="card-footer small text-muted text-end">{relativeTime}</div>
       </div>
     </div>
   );
